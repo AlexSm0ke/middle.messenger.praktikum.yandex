b173c56cb7b1ea700a3c135e0f6154f4a7e7f932
PtitsynAA <ptitsynaa@alfastrah.ru>
2023-02-14 09:58:39 +0300

    для ревью

---
+++ src/components/ui/input/index.js
@@ -0,0 +1,18 @@
+import input from './input.hbs';
+import Handlebars from 'handlebars/dist/handlebars.runtime';
+
+Handlebars.registerPartial('input', input);
+
+window.addEventListener('DOMContentLoaded', () => {
+	const input_input = document.querySelector('.input-container__input');
+
+	const placeholder = document.querySelector('.input-container__placeholder');
+
+	input_input.addEventListener('focus', () => {
+		placeholder.className = 'input-container__placeholder_modified';
+	})
+
+})
+
+
+
