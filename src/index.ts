cd2e09ef6d88b7cf62cecf8191697ccd5e85439b
PtitsynAA <ptitsynaa@alfastrah.ru>
2023-04-03 16:40:32 +0300

    устранил ошибки стайллинта для проходжения автотестов

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,6 @@ import Block from './utils/Block';
 
 const currentPath: string = window.location.pathname;
 
-// window.addEventListener('DOMContentLoaded', () => {
-
 const pages: { [key: string]: Block } = {
 	[routes.home.path]: MainPage,
 	[routes.login.path]: new LoginPage({}),
@@ -31,23 +29,11 @@ const pages: { [key: string]: Block } = {
 	[routes.error_500.path]: Page500
 }
 
-// const routing = {
-// 	'/': MainPage,
-// 	"/login": LoginPage,
-// 	"/register": SingUpPage,
-// 	"/chat": ChatPage,
-// 	"/profile": ProfilePage,
-// 	"/profile-edit": ProfileEditPage,
-// 	// "/password-edit":PasswordEditPage,
-// 	"/404": new ErrorPage({}),
-// 	"/500": Page500,
-// }
-
 const renderDOM = (elem: string, block: Block) => {
 	const root = document.querySelector(elem);
 
 	root!.innerHTML = '';
-	root!.appendChild(block.getContent());
+	root!.appendChild(block.getContent() as HTMLElement);
 	block.dispatchComponentDidMount();
 };
 
@@ -56,4 +42,3 @@ Object.entries(pages).forEach(([url, page]) => {
 		renderDOM('#app', page);
 	}
 });
-// })
